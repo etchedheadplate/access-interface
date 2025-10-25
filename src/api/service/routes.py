5c4df473f48475ad296643d8f8ca50fb87f402ac
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2025-10-25 16:38:23 +0300

    Created service package

---
+++ src/api/service/routes.py
@@ -0,0 +1,16 @@
+import httpx
+from dotenv import load_dotenv
+from fastapi import APIRouter
+
+from src.api.config import DB_BASE, DBRoutes
+
+load_dotenv()
+
+router_service = APIRouter(tags=["service"])
+
+
+@router_service.get("/ping_db")
+async def send_ping():
+    async with httpx.AsyncClient() as client:
+        response = await client.post(DB_BASE + DBRoutes.SERVICE_PING)
+        return {"from_db_service": response.json()}
