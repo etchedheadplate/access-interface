5b0e26f86699017eb0744c8e87deae24207d9e59
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2025-10-30 00:27:30 +0300

    Added private endpoints

--- src/api/config.py
+++ src/api/config.py
@@ -1,5 +1,4 @@
 import os
-from dataclasses import dataclass
 
 from dotenv import load_dotenv
 
@@ -8,10 +7,43 @@ load_dotenv()
 DB_BASE = str(os.environ.get("DB_SERVICE_BASE_URL"))
 
 
-@dataclass
-class DBRoutes:
-    HEALTH_PING = "/ping"
+class Routes:
+    class Open:
+        PING = "/ping"
 
-    AUTH_REGISTER = "/auth/register"
-    AUTH_LOGIN = "/auth/login"
-    AUTH_CHECK = "/authenticated-route"
+        class Auth:
+            REGISTER = "/auth/register"
+            LOGIN = "/auth/login"
+            CHECK = "/auth"
+
+        class DB:
+            USERS = "/database/users/"
+            GROUPS = "/database/groups"
+            PERMISSIONS = "/database/permissions"
+            RESOURCES = "/database/resources"
+
+    class Private:
+        USER_IN_GROUP = "/access/check/user-in-group"
+        PERMISSION_IN_GROUP = "/access/check/permission-in-group"
+        RESOURCE_IN_PERMISSION = "/access/check/resource-in-permission"
+
+        class User:
+            GROUPS = "/access/view/user-groups"
+            PERMISSIONS = "/access/view/user-permissions"
+            RESOURCES = "/access/view/user-resources"
+
+        class Group:
+            USERS = "/access/view/group-users"
+            PERMISSIONS = "/access/view/group-permissions"
+            RESOURCES = "/access/view/group-resources"
+
+        class Permission:
+            ID = "/database/permissions/{permission_id}"
+            GROUPS = "/access/view/permission-groups"
+            USERS = "/access/view/permission-users"
+            RESOURCES = "/access/view/permission-resources"
+
+        class Resource:
+            USERS = "/access/view/resource-users"
+            GROUPS = "/access/view/resource-groups"
+            PERMISSIONS = "/access/view/resource-permissions"
