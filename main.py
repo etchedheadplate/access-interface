2edb414e938ab0307db0922cec90a104dc6ad2cb
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2025-10-30 00:34:35 +0300

    Added RabbitMQ init

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
+import asyncio
+from contextlib import asynccontextmanager
+from typing import Any
+
 from dotenv import load_dotenv
 from fastapi import FastAPI
 
-from src.api.auth.routes import router_auth
-from src.api.health.routes import router_health
+from src.api import router
+from src.logger import logger
+from src.queue import (
+    EXCHANGE_NAME,
+    ROUTING_KEY_STATUS,
+    ROUTING_KEY_TASK,
+    RabbitMQConnection,
+    RabbitMQConsumer,
+    RabbitMQProducer,
+)
 
 load_dotenv()
 
-app = FastAPI()
-app.include_router(router_health)
-app.include_router(router_auth)
+
+rabbit_connection = RabbitMQConnection()
+producer = RabbitMQProducer(rabbit_connection)
+consumer = RabbitMQConsumer(rabbit_connection)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await rabbit_connection.connect()
+
+    async def handle_message(msg: dict[str, Any]):
+        logger.info(f"Message received: {msg}")
+
+    asyncio.create_task(consumer.consume(EXCHANGE_NAME, ROUTING_KEY_TASK, handle_message))
+    asyncio.create_task(consumer.consume(EXCHANGE_NAME, ROUTING_KEY_STATUS, handle_message))
+
+    yield
+
+    await rabbit_connection.close()
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router)
