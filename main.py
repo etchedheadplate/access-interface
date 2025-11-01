19afcbbd820364dbc8abdad02188c67a3e987411
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2025-11-01 10:45:53 +0300

    Added message handling to app lifespan

--- main.py
+++ main.py
@@ -23,16 +23,29 @@ rabbit_connection = RabbitMQConnection()
 producer = RabbitMQProducer(rabbit_connection)
 consumer = RabbitMQConsumer(rabbit_connection)
 
+last_status_message: dict[str, Any] | None = None
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await rabbit_connection.connect()
 
-    async def handle_message(msg: dict[str, Any]):
-        logger.info(f"Message received: {msg}")
+    async def handle_message(message: dict[str, Any], routing_key: str):
+        if routing_key == ROUTING_KEY_TASK:
+            logger.info(
+                f"Message received: routing_key={routing_key},   request_id={message['request_id']}, type={message['request_type']}"
+            )
+        elif routing_key == ROUTING_KEY_STATUS:
+            logger.info(
+                f"Message received: routing_key={routing_key}, request_id={message['request_id']}, status={message['request_status']}"
+            )
 
-    asyncio.create_task(consumer.consume(EXCHANGE_NAME, ROUTING_KEY_TASK, handle_message))
-    asyncio.create_task(consumer.consume(EXCHANGE_NAME, ROUTING_KEY_STATUS, handle_message))
+    asyncio.create_task(
+        consumer.consume(EXCHANGE_NAME, ROUTING_KEY_TASK, lambda msg: handle_message(msg, ROUTING_KEY_TASK))
+    )
+    asyncio.create_task(
+        consumer.consume(EXCHANGE_NAME, ROUTING_KEY_STATUS, lambda msg: handle_message(msg, ROUTING_KEY_STATUS))
+    )
 
     yield
 
@@ -41,3 +54,5 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(router)
+
+app.state.last_status_message = lambda: last_status_message
