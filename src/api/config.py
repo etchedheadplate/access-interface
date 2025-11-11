50a0974417a9c6bfe19af96346a9ceec5c9c1f98
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2025-11-11 23:44:01 +0300

    Moved project config

--- src/api/config.py
+++ src/api/config.py
@@ -9,48 +9,48 @@ DB_BASE: str = str(os.environ.get("DB_SERVICE_BASE_URL"))
 
 class Routes:
     class Open:
-        PING: str = "/ping"
+        # PING: str = "/ping"
 
         class Auth:
-            REGISTER: str = "/auth/register"
-            LOGIN: str = "/auth/login"
+            # REGISTER: str = "/auth/register"
+            # LOGIN: str = "/auth/login"
             CHECK: str = "/auth"
 
-        class DB:
-            USERS: str = "/database/users/"
-            GROUPS: str = "/database/groups/"
-            PERMISSIONS: str = "/database/permissions/"
-            RESOURCES: str = "/database/resources/"
+        # class DB:
+        # USERS: str = "/database/users/"
+        # GROUPS: str = "/database/groups/"
+        # PERMISSIONS: str = "/database/permissions/"
+        # RESOURCES: str = "/database/resources/"
 
-            USER_GROUPS: str = "/access/view/user-groups"
-            USER_PERMISSIONS: str = "/access/view/user-permissions"
-            USER_RESOURCES: str = "/access/view/user-resources"
+        ### USER_GROUPS: str = "/access/view/user-groups"
+        ### USER_PERMISSIONS: str = "/access/view/user-permissions"
+        ### USER_RESOURCES: str = "/access/view/user-resources"
 
     class Private:
         USER_IN_GROUP: str = "/access/check/user-in-group"
         PERMISSION_IN_GROUP: str = "/access/check/permission-in-group"
         RESOURCE_IN_PERMISSION: str = "/access/check/resource-in-permission"
 
-        class User:
-            NAME: str = "/database/users/{user_id}"
-            GROUPS: str = "/access/view/user-groups"
-            PERMISSIONS: str = "/access/view/user-permissions"
-            RESOURCES: str = "/access/view/user-resources"
+        # class User:
+        # NAME: str = "/database/users/{user_id}"
+        ### GROUPS: str = "/access/view/user-groups"
+        ### PERMISSIONS: str = "/access/view/user-permissions"
+        ### RESOURCES: str = "/access/view/user-resources"
 
-        class Group:
-            NAME: str = "/database/groups/{group_id}"
-            USERS: str = "/access/view/group-users"
-            PERMISSIONS: str = "/access/view/group-permissions"
-            RESOURCES: str = "/access/view/group-resources"
+        # class Group:
+        # NAME: str = "/database/groups/{group_id}"
+        # USERS: str = "/access/view/group-users"
+        # PERMISSIONS: str = "/access/view/group-permissions"
+        # RESOURCES: str = "/access/view/group-resources"
 
-        class Permission:
-            NAME: str = "/database/permissions/{permission_id}"
-            GROUPS: str = "/access/view/permission-groups"
-            USERS: str = "/access/view/permission-users"
-            RESOURCES: str = "/access/view/permission-resources"
+        # class Permission:
+        # NAME: str = "/database/permissions/{permission_id}"
+        # GROUPS: str = "/access/view/permission-groups"
+        # USERS: str = "/access/view/permission-users"
+        # RESOURCES: str = "/access/view/permission-resources"
 
-        class Resource:
-            NAME: str = "/database/resources/{resource_id}"
-            USERS: str = "/access/view/resource-users"
-            GROUPS: str = "/access/view/resource-groups"
-            PERMISSIONS: str = "/access/view/resource-permissions"
+        # class Resource:
+        # NAME: str = "/database/resources/{resource_id}"
+        # USERS: str = "/access/view/resource-users"
+        # GROUPS: str = "/access/view/resource-groups"
+        # PERMISSIONS: str = "/access/view/resource-permissions"
---
+++ src/config.py
@@ -0,0 +1,23 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    SERVICE_NAME: str
+
+    DB_SERVICE_BASE_URL: str
+
+    RABBITMQ_HOST: str
+    RABBITMQ_PORT: int
+    RABBITMQ_USER: str
+    RABBITMQ_PASSWORD: str
+    RABBITMQ_VHOST: str
+
+    EXCHANGE_NAME: str
+    ROUTING_KEY_TASK: str
+    ROUTING_KEY_STATUS_CREATED: str
+    ROUTING_KEY_STATUS_VALIDATED: str
+    ROUTING_KEY_STATUS_REJECTED: str
+    ROUTING_KEY_STATUS_DONE: str
+    ROUTING_KEY_STATUS_UNPROCESSABLE: str
+
+    model_config = SettingsConfigDict(env_file=".env")
