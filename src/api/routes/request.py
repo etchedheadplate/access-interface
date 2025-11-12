d1d1669e62dcdd8956594390843d519267c3e9e6
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2025-11-12 22:30:58 +0300

    Encapsulated worker logic

--- src/api/routes/request.py
+++ src/api/routes/request.py
@@ -1,21 +1,12 @@
 from uuid import UUID
 
-from dotenv import load_dotenv
 from fastapi import APIRouter, Depends, Request
 from pydantic import PositiveInt
 
 from src.api.dependancies import oauth2_scheme
-from src.logger import logger
-from src.queue import EXCHANGE_NAME, ROUTING_KEY_STATUS_CREATED, ROUTING_KEY_TASK, send_message
-from src.services.status.schemas import (
-    BaseStatus,
-    StatusCreatedResponse,
-    StatusNotFoundResponse,
-    StatusUnprocessableResponse,
-)
+from src.services.status.schemas import BaseStatus, StatusNotFoundResponse
 from src.services.tasks.creators import get_task_creator
-
-load_dotenv()
+from src.worker import RequestWorker
 
 router = APIRouter(prefix="/request", tags=["Request"])
 create_router = APIRouter(prefix="/create")
@@ -32,116 +23,56 @@ async def check_request_status(request: Request, request_id: str, token: str = D
 
 @create_router.post("/add-to-group", response_model=BaseStatus)
 async def request_add_user_to_group(user_id: UUID, group_id: PositiveInt, token: str = Depends(oauth2_scheme)):
-    request_type = "join_group"
-    request = get_task_creator(request_type, user_id)
-    task = await request.create(group_id=group_id)
-    if not task.error:
-        await send_message(EXCHANGE_NAME, ROUTING_KEY_TASK, task.model_dump())
-        logger.info(f"OUT: request_id={task.request_id}, request_status={task.request_type}")
-        message_out = StatusCreatedResponse(request_id=task.request_id)
-    else:
-        message_out = StatusUnprocessableResponse(request_id="")
-
-    await send_message(EXCHANGE_NAME, ROUTING_KEY_STATUS_CREATED, message_out.model_dump())
-    logger.info(f"OUT: request_id={message_out.request_id}, request_status={message_out.request_status}")
-
-    return message_out
+    creator = get_task_creator("join_group", user_id)
+    task = await creator.create(group_id=group_id)
+    worker = RequestWorker(task)
+    return await worker.send_request()
 
 
 @create_router.post("/remove-from-group", response_model=BaseStatus)
 async def request_remove_user_from_group(user_id: UUID, group_id: PositiveInt, token: str = Depends(oauth2_scheme)):
-    request_type = "exclude_from_group"
-    request = get_task_creator(request_type, user_id)
-    task = await request.create(group_id=group_id)
-    if not task.error:
-        await send_message(EXCHANGE_NAME, ROUTING_KEY_TASK, task.model_dump())
-        logger.info(f"OUT: request_id={task.request_id}, request_status={task.request_type}")
-        message_out = StatusCreatedResponse(request_id=task.request_id)
-    else:
-        message_out = StatusUnprocessableResponse(request_id="")
-
-    await send_message(EXCHANGE_NAME, ROUTING_KEY_STATUS_CREATED, message_out.model_dump())
-    logger.info(f"OUT: request_id={message_out.request_id}, request_status={message_out.request_status}")
-
-    return message_out
+    creator = get_task_creator("exclude_from_group", user_id)
+    task = await creator.create(group_id=group_id)
+    worker = RequestWorker(task)
+    return await worker.send_request()
 
 
 @create_router.post("/view-user-groups", response_model=BaseStatus)
 async def request_view_user_groups(user_id: UUID, token: str = Depends(oauth2_scheme)):
-    request_type = "view_user_groups"
-    request = get_task_creator(request_type, user_id)
-    task = await request.create()
-    if not task.error:
-        await send_message(EXCHANGE_NAME, ROUTING_KEY_TASK, task.model_dump())
-        logger.info(f"OUT: request_id={task.request_id}, request_status={task.request_type}")
-        message_out = StatusCreatedResponse(request_id=task.request_id)
-    else:
-        message_out = StatusUnprocessableResponse(request_id="")
-
-    await send_message(EXCHANGE_NAME, ROUTING_KEY_STATUS_CREATED, message_out.model_dump())
-    logger.info(f"OUT: request_id={message_out.request_id}, request_status={message_out.request_status}")
-
-    return message_out
+    creator = get_task_creator("view_user_groups", user_id)
+    task = await creator.create()
+    worker = RequestWorker(task)
+    return await worker.send_request()
 
 
 @create_router.post("/add-permission", response_model=BaseStatus)
 async def request_add_permission_to_user(
     user_id: UUID, permission_id: PositiveInt, token: str = Depends(oauth2_scheme)
 ):
-    request_type = "access_permission"
-    request = get_task_creator(request_type, user_id)
-    task = await request.create(permission_id=permission_id)
-    if not task.error:
-        await send_message(EXCHANGE_NAME, ROUTING_KEY_TASK, task.model_dump())
-        logger.info(f"OUT: request_id={task.request_id}, request_status={task.request_type}")
-        message_out = StatusCreatedResponse(request_id=task.request_id)
-    else:
-        message_out = StatusUnprocessableResponse(request_id="")
-
-    await send_message(EXCHANGE_NAME, ROUTING_KEY_STATUS_CREATED, message_out.model_dump())
-    logger.info(f"OUT: request_id={message_out.request_id}, request_status={message_out.request_status}")
-
-    return message_out
+    creator = get_task_creator("access_permission", user_id)
+    task = await creator.create(permission_id=permission_id)
+    worker = RequestWorker(task)
+    return await worker.send_request()
 
 
 @create_router.post("/remove-permission", response_model=BaseStatus)
 async def request_remove_permission_from_user(
     user_id: UUID, permission_id: PositiveInt, token: str = Depends(oauth2_scheme)
 ):
-    request_type = "remove_permission"
-    request = get_task_creator(request_type, user_id)
-    task = await request.create(permission_id=permission_id)
-    if not task.error:
-        await send_message(EXCHANGE_NAME, ROUTING_KEY_TASK, task.model_dump())
-        logger.info(f"OUT: request_id={task.request_id}, request_status={task.request_type}")
-        message_out = StatusCreatedResponse(request_id=task.request_id)
-    else:
-        message_out = StatusUnprocessableResponse(request_id="")
-
-    await send_message(EXCHANGE_NAME, ROUTING_KEY_STATUS_CREATED, message_out.model_dump())
-    logger.info(f"OUT: request_id={message_out.request_id}, request_status={message_out.request_status}")
-
-    return message_out
+    creator = get_task_creator("remove_permission", user_id)
+    task = await creator.create(permission_id=permission_id)
+    worker = RequestWorker(task)
+    return await worker.send_request()
 
 
 @create_router.post("/view-resource-permission", response_model=BaseStatus)
 async def request_view_resource_permission(
     user_id: UUID, resource_id: PositiveInt, token: str = Depends(oauth2_scheme)
 ):
-    request_type = "get_resource_permission"
-    request = get_task_creator(request_type, user_id)
-    task = await request.create(resource_id=resource_id)
-    if not task.error:
-        await send_message(EXCHANGE_NAME, ROUTING_KEY_TASK, task.model_dump())
-        logger.info(f"OUT: request_id={task.request_id}, request_status={task.request_type}")
-        message_out = StatusCreatedResponse(request_id=task.request_id)
-    else:
-        message_out = StatusUnprocessableResponse(request_id="")
-
-    await send_message(EXCHANGE_NAME, ROUTING_KEY_STATUS_CREATED, message_out.model_dump())
-    logger.info(f"OUT: request_id={message_out.request_id}, request_status={message_out.request_status}")
-
-    return message_out
+    creator = get_task_creator("get_resource_permission", user_id)
+    task = await creator.create(resource_id=resource_id)
+    worker = RequestWorker(task)
+    return await worker.send_request()
 
 
 router.include_router(create_router)
